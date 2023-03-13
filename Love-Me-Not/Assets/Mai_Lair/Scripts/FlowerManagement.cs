39caa33983829181a9d75bdfb82267437a7c3b1d
DinosaurPower <lysym685@newschool.edu>
2023-03-13 18:13:56 -0400

    Imported TMPro

--- Love-Me-Not/Assets/Mai_Lair/Scripts/FlowerManagement.cs
+++ Love-Me-Not/Assets/Mai_Lair/Scripts/FlowerManagement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace godzillabanana{
 public class FlowerManagement : MonoBehaviour
 {
     public GameObject[] petals;
     public KeyCode[] keycode;
+    public TMP_Text Divination;
+    public string[] divinationOptions;
     // Start is called before the first frame update
     void Start()
     {
