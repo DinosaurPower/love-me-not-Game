21bf787347fb13ecd1cae3e0224bc397f0bf45ef
DinosaurPower <lysym685@newschool.edu>
2023-03-13 18:09:08 -0400

    set up test petals and flower

---
+++ Love-Me-Not/Assets/Mai_Lair/Scripts/FlowerManagement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace godzillabanana{
+public class FlowerManagement : MonoBehaviour
+{
+    public GameObject[] petals;
+    public KeyCode[] keycode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
